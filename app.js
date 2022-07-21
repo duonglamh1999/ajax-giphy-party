5a21f77cd68b8d88da6e73e931fe84d64402824e
duonglamh1999 <duonglamh1999@gmail.com>
2022-07-21 18:20:22 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const $gifContainer = $('.gif_section_container');
+const $searchInput = $('#form_input');
+const $form = $('form');
+const $deleteBtn = $('#delete');
+
+$form.on('submit', async function(e){
+    e.preventDefault();
+    const searchTerm  = $searchInput.val()
+    const response = await getGifData(searchTerm);
+    makeNewDiv(response)
+    $searchInput.val("");
+})
+
+function getGifData(searchTerm){
+   const response = axios.get("http://api.giphy.com/v1/gifs/search",{params:{q:searchTerm,
+    api_key:'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym'}})
+    return response
+}
+function randomNum(response){
+   return Math.floor(Math.random() * response.data.data.length)
+}
+function makeNewDiv(response){
+    const randomIndx = randomNum(response);
+
+    const $newDiv  = $("<div>",{class:'gif_container'})
+    const $newImg = $("<img>", {src: response.data.data[randomIndx].images.original.url}) 
+    $newDiv.append($newImg)
+    $gifContainer.append($newDiv);
+}
+$deleteBtn.on('click', e=> {
+    $gifContainer.empty();
+})
