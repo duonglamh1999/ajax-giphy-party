6a650a2c964c573fdba466d6f1131ffc646512e3
duonglamh1999 <duonglamh1999@gmail.com>
2022-07-21 18:41:38 -0500

    fix API link

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ $form.on('submit', async function(e){
 })
 
 function getGifData(searchTerm){
-   const response = axios.get("https://api.giphy.com/v1/gifs/search",{params:{q:searchTerm,
+   const response = axios.get("//api.giphy.com/v1/gifs/search",{params:{q:searchTerm,
     api_key:'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym'}})
     return response
 }
